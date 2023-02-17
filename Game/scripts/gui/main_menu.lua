6d1b478aa6bc215c77676036beb37667c300b24d
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2023-02-18 00:53:55 +0300

    imgui tests

--- Game/scripts/gui/main_menu.lua
+++ Game/scripts/gui/main_menu.lua
@@ -3,5 +3,8 @@ local imgui = require 'imgui'
 return function (time)
 	imgui.Begin('Test')
 	imgui.Text('Hello, world!')
+	if imgui.Button('Hello, world!') then
+		print('Hello, world!')
+	end
 	imgui.End()
 end
